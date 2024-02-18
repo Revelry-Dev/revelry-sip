e92646046554dbc30c81bc41eb2ef79d664faed5
Jamie <jmeleacc@gmail.com>
2024-02-18 09:28:58 -0600

    Fix

--- revelry-sip.php
+++ revelry-sip.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Stick Important Pages to the Top
+Plugin Name: Revelry Stick Important Pages
 Plugin URI: https://revelry.dev/plugins/sip
 Plugin Author: https://revelry.dev
 Description: Automatically Stick your Front Page and Blog Posts page to the top of the pages list.
